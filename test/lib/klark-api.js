aca6b0ac0d6939a699fa21bfcd4e96c2f41920a0
Ioannis Apostolidis <i.apostolidis@encodegroup.com>
2016-12-23 12:49:10 +0200

    testing and verbose logger

--- test/lib/klark-api.js
+++ test/lib/klark-api.js
@@ -26,6 +26,61 @@ describe('Normal plugins', function() {
     }, cb);
   });
 
+  it('Should properly get internal modules', function(cb) {
+    var klarkPrms = runKlarkOnSamples();
+    expectPrms.success(klarkPrms, function(klarkApi) {
+      var genInner1 = klarkApi.getModule('normalPluginsPlugin1');
+      var externalInner1 = klarkApi.getExternalModule('normalPluginsPlugin1');
+      var internalInner1 = klarkApi.getInternalModule('normalPluginsPlugin1');
+      expect(genInner1.log).to.equal('plugin1');
+      expect(genInner1).to.equal(internalInner1);
+      expect(externalInner1).to.equal(undefined);
+    }, cb);
+  });
+
+  it('Should properly inject an external module', function(cb) {
+    var klarkPrms = runKlarkOnSamples();
+    expectPrms.success(klarkPrms, function(klarkApi) {
+      var globby = klarkApi.injectExternalModule('globby');
+      expect(_.isFunction(globby.sync)).to.equal(true);
+      var externalGlobby = klarkApi.getExternalModule('globby');
+      expect(externalGlobby).to.equal(globby);
+    }, cb);
+  });
+
+  it('Should properly inject an internal module from metadata', function(cb) {
+    var api;
+    runKlarkOnSamples()
+      .then(function(klarkApi) {
+        api = klarkApi;
+        return api.injectInternalModuleFromMetadata('injectedPlugin', function($lodash, normalPluginsPlugin1) {
+          return {
+            log: 'injected',
+            _: $lodash,
+            normalPluginsPlugin1: normalPluginsPlugin1
+          };
+        });
+      }).then(function(instance) {
+        expect(instance.log).to.equal('injected');
+        var internalPlug = api.getInternalModule('injectedPlugin');
+        expect(internalPlug).to.equal(instance);
+      })
+      .then(function() { cb(); })
+      .catch(function(reason) { cb(reason); });
+  });
+
+  it('Should properly get the application dependencies graph', function(cb) {
+    var klarkPrms = runKlarkOnSamples();
+    expectPrms.success(klarkPrms, function(klarkApi) {
+      var dependenciesGraph = klarkApi.getApplicationDependenciesGraph();
+      expect(_.keys(dependenciesGraph)).to.deep.equal(['normalPluginsPlugin1', 'normalPluginsPlugin2', 'normalPluginsPlugin3']);
+      expect(dependenciesGraph.normalPluginsPlugin1).to.deep.equal([{
+          isExternal: true,
+          name: 'lodash'
+      }]);
+    }, cb);
+  });
+
   function runKlarkOnSamples() {
     return klark.run({
       predicateFilePicker: function() {
